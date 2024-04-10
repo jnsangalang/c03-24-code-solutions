0fd9ca38c3186e5f6d3d7facfbdba7b869eec079
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-10 21:38:27 +0000

    index css

---
+++ typescript-typing-tutor/main.js
@@ -0,0 +1,29 @@
+'use strict';
+// const sentence ='Today is a good day';
+// const $phrase = document.querySelector('.phrase');
+// const $restart = document.querySelector('.restart');
+// const $input = document.querySelector('.input');
+// const $spanEl = document.querySelectorAll('span');
+// if (!$phrase) throw new Error('The $phrase query failed');
+// if (!$restart) throw new Error('The $restart query failed');
+// if (!$input) throw new Error('The $input query failed');
+// if (!$spanEl) throw new Error('The $spanEl query failed');
+// let i = 0;
+// $spanEl.forEach(function(char:string,index:number){
+// console.log('index',index);
+// console.log('char',char);
+// })
+// $input.addEventListener('keydown', () =>{
+//   let errorCount =0;
+//   i++;
+//     if($input.textContent === $phrase.textContent){
+//       $input.classList.add('correct');
+//     }
+//     else{
+//       $phrase.classList.add('incorrect');
+//       errorCount++;
+//     }
+//     if ($input.textContent.length === sentence.length && errorCount === 0) {
+//       alert('Done');
+//     }
+//   })
