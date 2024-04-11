998e63a843b7781c67fcdd9f091c5277d50871bd
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-11 17:30:22 +0000

    completed typescript typing tutor

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,22 +1,19 @@
 'use strict';
-// const $phrase = document.querySelector('.phrase');
-// const $restart = document.querySelector('.restart');
-// const $input = document.querySelector('.input');
-// const $spanEl = document.querySelectorAll('span');
-// if (!$phrase) throw new Error('The $phrase query failed');
-// if (!$restart) throw new Error('The $restart query failed');
-// if (!$input) throw new Error('The $input query failed');
-// if (!$spanEl) throw new Error('The $spanEl query failed');
-// $input.addEventListener('keydown', (event:Event) =>{
-//   let errorCount = 0;
-//     if((event as KeyboardEvent).key === $phrase.textContent){
-//       $input.classList.add('correct');
-//     }
-//     else{
-//       $phrase.classList.add('incorrect');
-//       errorCount++;
-//     }
-//     if (errorCount === 0) {
-//       alert('Done');
-//     }
-//   })
+const $phrase = document.querySelector('.phrase');
+const $restart = document.querySelector('.restart');
+const $input = document.querySelector('.input');
+const $spanEl = document.querySelectorAll('span');
+if (!$phrase) throw new Error('The $phrase query failed');
+if (!$restart) throw new Error('The $restart query failed');
+if (!$input) throw new Error('The $input query failed');
+if (!$spanEl) throw new Error('The $spanEl query failed');
+console.log('span:', $spanEl);
+let currentChar = 0;
+document.addEventListener('keydown', (event) => {
+  if (event.key === $spanEl[currentChar].textContent) {
+    $spanEl[currentChar].classList.add('correct');
+    currentChar++;
+  } else {
+    $spanEl[currentChar].classList.add('incorrect');
+  }
+});
--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,26 +1,20 @@
-// const $phrase = document.querySelector('.phrase');
-// const $restart = document.querySelector('.restart');
-// const $input = document.querySelector('.input');
-// const $spanEl = document.querySelectorAll('span');
+const $phrase = document.querySelector('.phrase');
+const $restart = document.querySelector('.restart');
+const $input = document.querySelector('.input');
+const $spanEl = document.querySelectorAll('span');
 
-// if (!$phrase) throw new Error('The $phrase query failed');
-// if (!$restart) throw new Error('The $restart query failed');
-// if (!$input) throw new Error('The $input query failed');
-// if (!$spanEl) throw new Error('The $spanEl query failed');
+if (!$phrase) throw new Error('The $phrase query failed');
+if (!$restart) throw new Error('The $restart query failed');
+if (!$input) throw new Error('The $input query failed');
+if (!$spanEl) throw new Error('The $spanEl query failed');
+console.log('span:', $spanEl);
+let currentChar = 0;
 
-// let currentChar=0;
-
-// $input.addEventListener('keydown', (event:KeyboardEvent) =>{
-//   let errorCount = 0;
-//     if((event).key === $spanEl[currentChar].textContent){
-//       $spanEl[currentChar].classList.add('correct');
-//       currentChar++;
-//     }
-//     else{
-//       $spanEl[currentChar].classList.add('incorrect');
-//       errorCount++;
-//     }
-//     if (errorCount === 0) {
-//       alert('Done');
-//     }
-//   })
+document.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (event.key === $spanEl[currentChar].textContent) {
+    $spanEl[currentChar].classList.add('correct');
+    currentChar++;
+  } else {
+    $spanEl[currentChar].classList.add('incorrect');
+  }
+});
