52e3589de52ae102db612d33b3172a9393d095e2
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-25 23:30:43 +0000

    completed fetch

---
+++ fetch/main.js
@@ -0,0 +1,37 @@
+'use strict';
+async function retrieveUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'GET',
+  });
+  try {
+    const users = await response.json();
+    console.log('Array of users:', users);
+  } catch {
+    if (!response.ok) {
+      const message = `Failed to get users, Error ${response.status}`;
+      console.log(message);
+      throw new Error(message);
+    }
+  }
+}
+async function retrievePokemon() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/umbreon/', {
+    method: 'GET',
+  });
+  try {
+    const pokemon = await response.json();
+    console.log('Favorite pokemon:', pokemon);
+  } catch {}
+  if (!response.ok) {
+    const message = `Failed to get favorite pokemon, Error ${response.status}`;
+    console.log(message);
+    throw new Error(message);
+  }
+}
+// try{
+retrieveUsers();
+retrievePokemon();
+// }
+// catch(error){
+//     console.log(error);
+// };
