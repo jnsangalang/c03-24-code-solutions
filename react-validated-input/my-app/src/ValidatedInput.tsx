2724c27517177856eec44e1a876f9c5968a8d7ee
Johan Sangalang <jnsangalang93@yahoo.com>
2024-05-16 18:23:27 +0000

    completed react validated input

--- react-validated-input/my-app/src/ValidatedInput.tsx
+++ react-validated-input/my-app/src/ValidatedInput.tsx
@@ -1,22 +1,46 @@
-import { useState } from "react"
-export function ValidatedInput(){
-  const [isValue, setIsValue] = useState(0);
+import React, { useState } from 'react';
+import './ValidatedInput.css';
+import { FaCheck } from 'react-icons/fa';
+import { FaXmark } from 'react-icons/fa6';
 
-  function handleChange(){
-    const value = 0
+export function ValidatedInput() {
+  const [isValue, setIsValue] = useState('');
+  let classNameCheck = 'hidden check';
+  let classNameXMark = 'x-mark';
+  let resposiveMessage = 'Password Required';
 
-    return value
-
+  if (isValue.length > 0 && isValue.length < 8) {
+    resposiveMessage = 'Password too short';
+    classNameCheck = 'hidden check';
+    classNameXMark = 'x-mark';
   }
-  const value = 0
-  return(
-  <div>
-    <input
-    name='password'
-    id='password'
-    value={value}
-    onChange={handleChange}></input>
-    <div>hi</div>
-  </div>
-)
+  if (isValue.length >= 8) {
+    resposiveMessage = '';
+    classNameCheck = 'check';
+    classNameXMark = 'hidden';
+  }
+
+  return (
+    <div className="flex w-full m-12">
+      <label>
+        <span className="header">Password</span>
+        <div className="relative w-80">
+          <div className="icons">
+            <FaCheck className={classNameCheck} />
+            <FaXmark className={classNameXMark} />
+          </div>
+          <input
+            name="password"
+            className="password"
+            value={isValue}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+              setIsValue(e.target.value)
+            }
+          />
+          <div className="absolute inset-y-0 right-2 flex items-center" />
+        </div>
+        <div className="p-1 text-red-700">{resposiveMessage}</div>
+      </label>
+    </div>
+  );
 }
