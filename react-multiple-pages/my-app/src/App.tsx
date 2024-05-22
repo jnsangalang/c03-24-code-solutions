ec719fd77b1886b12222231535d0c8c45667a692
Johan Sangalang <jnsangalang93@yahoo.com>
2024-05-22 15:18:13 +0000

    completed react-multiple-pages

---
+++ react-multiple-pages/my-app/src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react';
+import { About } from './pages/About';
+import { Dashboard } from './pages/Dashboard';
+import { Header } from './components/Header';
+import { NotFound } from './pages/NotFound';
+import { Details } from './pages/Details';
+import { Routes, Route } from 'react-router-dom';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route path="/about" element={<About />} />
+        <Route index element={<Dashboard />} />
+        <Route path="/details/:itemId" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
