0b338d596d23bda596ad7e17d3d7f8b1b953a9d4
Johan Sangalang <jnsangalang93@yahoo.com>
2024-05-30 18:46:55 +0000

    fixed errors

--- express-routes/server.ts
+++ express-routes/server.ts
@@ -4,8 +4,23 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('The date is', new Date(), req.method, req.path);
-  res.send('potato');
   next();
+});
+
+app.get('/', (req, res) => {
+  console.log('The date is', new Date(), req.method, req.path);
+  console.log('/ is the path');
+  res.send('/ is the path');
+});
+app.get('/notes', (req, res) => {
+  console.log('The date is', new Date(), req.method, req.path);
+  console.log('notes is the path');
+  res.send('notes is the path');
+});
+app.post('/notes/123', (req, res) => {
+  console.log('The date is', new Date(), req.method, req.path);
+  console.log('/notes/123 is the path');
+  res.send('/notes/123 is the path');
 });
 
 app.listen(8080, () => {
