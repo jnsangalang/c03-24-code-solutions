d9775062ab32b030aab223901c6a1c6096afad6b
Johan Sangalang <jnsangalang93@yahoo.com>
2024-06-05 18:22:06 +0000

    edits to functions in read.ts

--- full-stack-practice/client/src/lib/read.ts
+++ full-stack-practice/client/src/lib/read.ts
@@ -1,4 +1,6 @@
-export async function readCatalog() {
+import { Product } from '../Catalog';
+
+export async function readCatalog(): Promise<Product[]> {
   const response = await fetch('/api/products');
   if (!response.ok) {
     throw new Error('result is invalid');
@@ -6,7 +8,7 @@ export async function readCatalog() {
   return await response.json();
 }
 
-export async function readProduct(productId: number) {
+export async function readProduct(productId: number): Promise<Product> {
   const response = await fetch(`/api/products/${productId}`);
   if (!response.ok) {
     throw new Error(`product ${productId} is invalid`);
